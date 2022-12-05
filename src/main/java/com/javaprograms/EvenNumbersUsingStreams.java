63cd58a5621465df955834256a3053af157238c0
RamaKrishna <ram.946@gmail.com>
2022-12-05 18:44:02 +0530

    FilterNCollect file

--- src/main/java/com/javaprograms/EvenNumbersUsingStreams.java
+++ src/main/java/com/javaprograms/EvenNumbersUsingStreams.java
@@ -1,2 +1,12 @@
-package com.javaprograms;public class EvenNumbersUsingStreams {
+package com.javaprograms;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class EvenNumbersUsingStreams {
+
+    public static void main(String[] args) {
+        List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 13, 15, 17, 19, 21, 23, 25, 50);
+        numbers.stream().filter(n -> n % 2 == 0).forEach(System.out::println);
+    }
 }
